8489077b5bb23ab8ed38f87e3acc9693a9baedb7
Nidhin Raj <jnraj@live.com>
2021-08-21 08:24:26 +0530

    Update gamerbot.py

--- gamerbot.py
+++ gamerbot.py
@@ -6,6 +6,10 @@ client = discord.Client()
 keywords1=["hi", "hello", "sup", "wassup"]
 keywords2=["poda", "sheay", "sheri", "pwoli"]
 keywords3=["livestream", "live", "stream"]
+keywords4=["morning"]
+keywords5=["evening"]
+keywords6=["night"]
+keywords7=["onam"]
 
 
 @client.event                              
@@ -24,5 +28,22 @@ async def on_message(message):
     if any(keyword in message.content.lower() for keyword in keywords3):
         msg = 'Hey {0.author.mention} check out MrGamerRajs past livestreams <#553394919625195570>! '.format(message)        
         await message.channel.send(msg)
+        
+    if any(keyword in message.content.lower() for keyword in keywords4):
+        msg = '{0.author.mention} Good morning! '.format(message)
+        await message.channel.send(msg) 
+        
+    if any(keyword in message.content.lower() for keyword in keywords5):
+        msg = '{0.author.mention} Good evening! '.format(message)
+        await message.channel.send(msg) 
+        
+    if any(keyword in message.content.lower() for keyword in keywords6):
+        msg = '{0.author.mention} Good night! '.format(message)
+        await message.channel.send(msg)
+        
+    if any(keyword in message.content.lower() for keyword in keywords7):
+        msg = '{0.author.mention} Wishing you a very happy and prosperous Onam Happy Onam!! '.format(message)
+        await message.channel.send(msg) 
+
 
 client.run('ODc4NDY0ODUwODQ0NTg1OTk0.YSBkJQ.9Ebh1g8Z4gvmvBGH5LJwThTMIK8')
