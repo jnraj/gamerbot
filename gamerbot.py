f974bffc7fd6b7b2de2e1f7494552af17ff3b8f5
Nidhin Raj <jnraj@live.com>
2021-08-26 18:33:21 +0530

    Update gamerbot.py

--- gamerbot.py
+++ gamerbot.py
@@ -44,46 +44,6 @@ async def on_message(message):
     if any(keyword in message.content.lower() for keyword in keywords7):
         msg = '{0.author.mention} Wishing you a very happy and prosperous Onam! '.format(message)
         await message.channel.send(msg) 
-        
-@client.command()
-async def pages(ctx):
-    contents = ["This is page 1!", "This is page 2!", "This is page 3!", "This is page 4!"]
-    pages = 4
-    cur_page = 1
-    message = await ctx.send(f"Page {cur_page}/{pages}:\n{contents[cur_page-1]}")
-    # getting the message object for editing and reacting
-
-    await message.add_reaction("◀️")
-    await message.add_reaction("▶️")
-
-    def check(reaction, user):
-        return user == ctx.author and str(reaction.emoji) in ["◀️", "▶️"]
-        # This makes sure nobody except the command sender can interact with the "menu"
-
-    while True:
-        try:
-            reaction, user = await client.wait_for("reaction_add", timeout=60, check=check)
-            # waiting for a reaction to be added - times out after x seconds, 60 in this
-            # example
-
-            if str(reaction.emoji) == "▶️" and cur_page != pages:
-                cur_page += 1
-                await message.edit(content=f"Page {cur_page}/{pages}:\n{contents[cur_page-1]}")
-                await message.remove_reaction(reaction, user)
-
-            elif str(reaction.emoji) == "◀️" and cur_page > 1:
-                cur_page -= 1
-                await message.edit(content=f"Page {cur_page}/{pages}:\n{contents[cur_page-1]}")
-                await message.remove_reaction(reaction, user)
-
-            else:
-                await message.remove_reaction(reaction, user)
-                # removes reactions if the user tries to go forward on the last page or
-                # backwards on the first page
-        except asyncio.TimeoutError:
-            await message.delete()
-            break
-            # ending the loop if user doesn't react after x seconds
 
 
 client.run('ODc4NDY0ODUwODQ0NTg1OTk0.YSBkJQ.9Ebh1g8Z4gvmvBGH5LJwThTMIK8')
